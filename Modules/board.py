e58abd92d08ed5117456a9bb306da4dd4ec2fde8
Rayyan Borg <42804334+MrRayyanBorg@users.noreply.github.com>
2021-10-13 13:36:57 +0000

    FIXED - Removal on Numpy

--- Modules/board.py
+++ Modules/board.py
@@ -1,37 +1,19 @@
-import numpy as np
-
 class Board:
-  def __init__(self):
-    pass
 
-  def create2dArray(self,vertical,horizontal):
-    vertical_array   = []
-    horizontal_array = []
-    for i in range(0,horizontal):
-      horizontal_array.append(0)
-    for i in range(0,vertical):
-      vertical_array.append(horizontal_array)
-    return vertical_array
-
-  def formatArray(self,array):
-    for item in array:
-      print(item)
-  
   #this function creates a board along an x,y
-  def create_board(self,rows,columns):
-    self.board = self.create2dArray(rows,columns) #creates board 6 by 7
-    return self.board
+  def create_board(rows,columns):
+    #board = np.zeros((rows,columns)) #creates board 6 by 7
+    board = [[0 for x in range(columns)] for y in range(rows)]
+    return board
 
   #this function outputs the board on a flipped axis
-  def print_board(self):
-    self.formatArray(self.board)
+  def print_board(board):
+    board = board[::-1]
+    for i in board:
+      print(i)
 
-  def get_next_open_row(self, rows, input_column):
+  def get_next_open_row(board, rows, input_column):
   #this function checks which row is not taken, closest to the first row and within input column
-    #print(rows[input_column])
-    for i in range(rows):
-      if self.board[i][input_column] == 0:
-        return i
-    
-        
-        
+    for row in range(rows):
+      if board[row][input_column] == 0:
+        return row
--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from Modules.board import Board
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
-def add_chip(board, row, selected_column, chip): 
+def add_chip(board, row, input_column, chip): 
   #Updates the board with locations of chips for each player
-  print(board[row][selected_column])
-  print("coc")
+  board[row][input_column] = chip
 
-def is_valid_location(board, selected_column):
+def is_valid_location(board, input_column):
   #this function validates if the first row is free in the input column
-  return board[ROW_COUNT-1][selected_column] == 0
+  return board[ROW_COUNT-1][input_column] == 0
 
-board = Board()
-board_axis = board.create_board(ROW_COUNT,COLUMN_COUNT) #creates the game board
-board.print_board() #outputs the board
+board = Board.create_board(ROW_COUNT,COLUMN_COUNT) #creates the game board
+Board.print_board(board) #outputs the board
 
 game_over = False #game over variable
 turn = 0 #alternating turn between players
@@ -25,21 +23,21 @@ while not game_over: #This will continue running until the game is over
   #Player1 turn
   if turn == 0:
     #taking input from the user as int
-    selected_column = int(input("Player 1 - Make your selection (0-6):")) 
+    input_column = int(input("Player 1 - Make your selection (0-6):")) 
 
-    if is_valid_location(board_axis, selected_column):
-      row = board.get_next_open_row(ROW_COUNT, selected_column)
-      add_chip(board_axis, row, selected_column, 1)
+    if is_valid_location(board, input_column):
+      row = Board.get_next_open_row(board, ROW_COUNT, input_column)
+      add_chip(board, row, input_column, 1)
 
   #Player2 turn
   else:
-    selected_column = int(input("Player 2 - Make your selection (0-6):")) 
+    input_column = int(input("Player 2 - Make your selection (0-6):")) 
 
-    if is_valid_location(board_axis, selected_column):
-      row = board.get_next_open_row(board_axis, ROW_COUNT, selected_column)
-      add_chip(board_axis, row, selected_column, 2)
+    if is_valid_location(board, input_column):
+      row = Board.get_next_open_row(board, ROW_COUNT, input_column)
+      add_chip(board, row, input_column, 2)
   
-  board.print_board()
+  Board.print_board(board)
   #alternates the turns between player 1 and player 2
   turn += 1
   turn %= 2
